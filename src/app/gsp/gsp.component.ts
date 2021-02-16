3543537a8019e505bc0604352d651cc77e47abc8
dashuai009 <1525836294@qq.com>
2021-02-16 20:49:19 +0800

    gsp

--- src/app/gsp/gsp.component.ts
+++ src/app/gsp/gsp.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var GGBApplet: any;
 @Component({
   selector: 'app-gsp',
   templateUrl: './gsp.component.html',
@@ -10,6 +10,17 @@ export class GspComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var params = {
+      "appName": "geometry",
+      "width": 1000,
+      "height": 500,
+      "showToolBar": true,
+      "showAlgebraInput": true,
+      "showMenuBar": true
+    };
+    var ggbApplet = new GGBApplet(params, true);
+    ggbApplet.setHTML5Codebase('https://cdn.geogebra.org/apps/5.0.534.0/web3d');
+    ggbApplet.inject('ggb-element');
   }
 
 }
