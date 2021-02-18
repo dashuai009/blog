b7f1122c32d1f1e306d70ed4ccc9d7e4f80ffc94
dashuai009 <1525836294@qq.com>
2021-02-18 18:39:50 +0800

    error.log

--- src/allArticleResources/make_json.py
+++ src/allArticleResources/make_json.py
@@ -1,5 +1,8 @@
 import json
 import os
+import sys
+fsock = open('error.log', 'w')
+sys.stderr = fsock
 
 list_dir = os.listdir("./")
 
@@ -11,11 +14,16 @@ for file in list_dir:
         lines = f.read()
         lines = lines.split("....------....-")
         # print(lines[0])
-        tmp = json.loads(lines[0])
-        if(tmp['title']+".md" != file):
-            print(tmp["title"], "文档title命名错误！")
+        try:
+            tmp = json.loads(lines[0])
+            if(tmp['title']+".md" != file):
+                fsock.write(tmp["title"] + "文档title命名错误！\n\n")
+        except Exception as e:
+            fsock.write("出错文件是：" + lines[0]+"\n\n")
+            fsock.write(str(e))
         list_item.append(lines[0])
         f.close()
 
 list_json.write("["+",".join(i for i in list_item)+"]")
 list_json.close()
+fsock.close()
